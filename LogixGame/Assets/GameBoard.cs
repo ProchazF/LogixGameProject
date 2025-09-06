fd2e12424c8e4c123c55f28fe68d5a4ba230e64a
Frantisek Prochazka <prochazf@seznam.cz>
2025-09-06 17:01:55 +0200

    updated logic for board

--- LogixGame/Assets/GameBoard.cs
+++ LogixGame/Assets/GameBoard.cs
@@ -1,31 +1,93 @@
+using System.Drawing;
 using UnityEngine;
 
-public enum CellState { Empty, Player1, Player2 }
-
+public enum MarbleColor
+{
+    None,       // empty
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Grey,       // joker
+    Black       // fixed in center at start
+}
 public class GameBoard : MonoBehaviour
 {
-    private CellState[,] board;
+    private MarbleColor[,] board;
 
     public int width, height;
 
-    public GameBoard(int w, int h)
+    public GameBoard(int width, int height)
     {
-        width = w;
-        height = h;
-        board = new CellState[w, h];
+        this.width = width;
+        this.height = height;
+        board = new MarbleColor[width, height];
+        InitializeBoard();
     }
 
-    public bool IsValidMove(int x, int y)
+    // Initialize the initial board state
+    private void InitializeBoard()
     {
-        return x >= 0 && y >= 0 && x < width && y < height && board[x, y] == CellState.Empty;
+        // Place black marble in the center
+        int widthCenter = width / 2;
+        int heightCenter = height / 2;
+        board[widthCenter, heightCenter] = MarbleColor.Black;
+    }
+    public MarbleColor GetMarble(int x, int y)
+    {
+        if (!IsValidCoord(x, y)) return MarbleColor.None;
+        return board[x, y];
     }
 
-    public bool ApplyMove(int x, int y, CellState player)
+    public bool PlaceMarble(int x, int y, MarbleColor color)
     {
-        if (!IsValidMove(x, y)) return false;
-        board[x, y] = player;
+        if (!IsValidCoord(x, y)) return false;
+        if (board[x, y] != MarbleColor.None) return false; // already occupied
+        board[x, y] = color;
         return true;
     }
 
-    public CellState GetCell(int x, int y) => board[x, y];
+    private bool IsValidCoord(int x, int y)
+    {
+        return x >= 0 && y >= 0 && x < width && y < height;
+    }
+
+    public bool IsAdjacentToAnyMarble(int x, int y)
+    {
+        if (!IsValidCoord(x, y)) return false;
+
+        // Check the 4 orthogonal directions
+        Vector2Int[] directions = {
+        new Vector2Int(0, 1),   // up
+        new Vector2Int(0, -1),  // down
+        new Vector2Int(1, 0),   // right
+        new Vector2Int(-1, 0)   // left
+    };
+
+        foreach (var dir in directions)
+        {
+            int nx = x + dir.x;
+            int ny = y + dir.y;
+            if (IsValidCoord(nx, ny) && board[nx, ny] != MarbleColor.None)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void PrintDebugBoard()
+    {
+        string debug = "";
+        for (int y = height - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                debug += board[x, y].ToString().Substring(0, 1) + " ";
+            }
+            debug += "\n";
+        }
+        Debug.Log(debug);
+    }
 }
