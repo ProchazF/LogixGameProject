72468d76aa715d1e50d44b3cf1aeb6fd1f3f2086
Frantisek Prochazka <prochazf@seznam.cz>
2025-09-08 10:26:37 +0200

    ad MarbleInventory file with logic

---
+++ LogixGame/Assets/MarbleInventory.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleInventory
+{
+    private Dictionary<MarbleColor, int> counts;
+
+    public MarbleInventory()
+    {
+        counts = new Dictionary<MarbleColor, int>
+        {
+            { MarbleColor.Red, 7 },
+            { MarbleColor.Blue, 7 },
+            { MarbleColor.Green, 7 },
+            { MarbleColor.Yellow, 7 },
+            { MarbleColor.Grey, 2 }
+        };
+    }
+
+    public bool Has(MarbleColor color)
+    {
+        return counts.ContainsKey(color) && counts[color] > 0;
+    }
+
+    // Use the color marble
+    public bool Use(MarbleColor color)
+    {
+        if (!Has(color)) return false;
+        counts[color]--;
+        return true;
+    }
+
+    // When replacing you add one back
+    public void AddBack(MarbleColor color)
+    {
+        if (!counts.ContainsKey(color)) return;
+        counts[color]++;
+    }
+
+    public int GetCount(MarbleColor color)
+    {
+        return counts.ContainsKey(color) ? counts[color] : 0;
+    }
+
+    public Dictionary<MarbleColor, int> GetAllCounts()
+    {
+        return new Dictionary<MarbleColor, int>(counts);
+    }
+}
